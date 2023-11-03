7770fa358b611e136b8a7fb5f473814c9ce27aa7
Alden Sayidina <aldraddd01@gmail.com>
2023-11-03 13:10:49 +0700

    Version 1.1

--- initiate.java
+++ initiate.java
@@ -1,6 +1,7 @@
 public class initiate {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.1");
+        System.out.println("...");
     }
 }
